c26551b1a948717f08e73e56097b8064cd225a5d
Honye <hongye.jun@qq.com>
2020-01-18 13:37:46 +0000

    CI built at 2020-01-18 13:37:46

---
+++ precache-manifest.9bafcb32cbc1c3703cd9bf2058f91f4a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db58e93b716537e5fb56ed1cc4317d4b",
+    "url": "/iask-site/index.html"
+  },
+  {
+    "revision": "848f3bdd7f95c5c489ad",
+    "url": "/iask-site/static/css/2.e8077076.chunk.css"
+  },
+  {
+    "revision": "b7d8bfd8cee9b7454bce",
+    "url": "/iask-site/static/css/main.3084cef4.chunk.css"
+  },
+  {
+    "revision": "848f3bdd7f95c5c489ad",
+    "url": "/iask-site/static/js/2.08b2348c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/iask-site/static/js/2.08b2348c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7d8bfd8cee9b7454bce",
+    "url": "/iask-site/static/js/main.7f3d8416.chunk.js"
+  },
+  {
+    "revision": "a662a31562b978db611d",
+    "url": "/iask-site/static/js/runtime-main.68273788.js"
+  },
+  {
+    "revision": "fce8d1ea0495b9cc259fea5734be550e",
+    "url": "/iask-site/static/media/book_demo.fce8d1ea.png"
+  },
+  {
+    "revision": "5b0bf5b10c4cdb8c0bd39bd1c448e823",
+    "url": "/iask-site/static/media/home_banner.5b0bf5b1.png"
+  },
+  {
+    "revision": "44f64ecb3d3d9115f3f105acfa7029d7",
+    "url": "/iask-site/static/media/logo.44f64ecb.svg"
+  },
+  {
+    "revision": "08f4f821ed150510ba48f71f77fd59b5",
+    "url": "/iask-site/static/media/project_demo.08f4f821.png"
+  },
+  {
+    "revision": "801b20cedb50ebacd9096b5dd8316c96",
+    "url": "/iask-site/static/media/qrcode_demo.801b20ce.png"
+  },
+  {
+    "revision": "5f5054bf1d9da6e821adeccd8c3c35ff",
+    "url": "/iask-site/static/media/service_arrow.5f5054bf.svg"
+  },
+  {
+    "revision": "85b902ba8ab6aa544250ee0f8cb7d9d4",
+    "url": "/iask-site/static/media/service_coach.85b902ba.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/iask-site/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/iask-site/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/iask-site/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/iask-site/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "756d6ad07a4c38b274fe72f7acb3399a",
+    "url": "/iask-site/static/media/slider_arrow.756d6ad0.svg"
+  },
+  {
+    "revision": "881c87c6f8c7a6578b2446a9e3ef4e41",
+    "url": "/iask-site/static/media/source_lesson.881c87c6.svg"
+  },
+  {
+    "revision": "7a7e248bdd2c2f0c5b87413769f0dc08",
+    "url": "/iask-site/static/media/source_online.7a7e248b.svg"
+  },
+  {
+    "revision": "1c8c68eeb86c14137510da689b9c4301",
+    "url": "/iask-site/static/media/source_publication.1c8c68ee.svg"
+  },
+  {
+    "revision": "243897c58336e3306e6d9eb00aee85be",
+    "url": "/iask-site/static/media/video_icon_arrow.243897c5.svg"
+  },
+  {
+    "revision": "17a2a3e70cdc5be7b2887dcb0ae265fa",
+    "url": "/iask-site/static/media/video_icon_play.17a2a3e7.svg"
+  },
+  {
+    "revision": "4a653619506bc3ae0f7f8f4829e27d62",
+    "url": "/iask-site/static/media/video_poster.4a653619.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64075d4571d61ed6e5c154d66eaf3de7.js"
+  "/iask-site/precache-manifest.9bafcb32cbc1c3703cd9bf2058f91f4a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iask-site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
