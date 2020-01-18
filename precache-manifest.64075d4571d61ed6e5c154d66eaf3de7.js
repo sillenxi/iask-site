3ea871970ac8030dccc3a247d6d05a3ef19f86db
Honye <hongye.jun@qq.com>
2020-01-18 12:13:06 +0000

    CI built at 2020-01-18 12:13:06

---
+++ precache-manifest.64075d4571d61ed6e5c154d66eaf3de7.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d676cf337ba1e6c75ed73e8f832c1291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a459a2820ad0a337af6b",
+    "url": "/static/css/2.2618107e.chunk.css"
+  },
+  {
+    "revision": "4ed4777a57547a782f15",
+    "url": "/static/css/main.02680220.chunk.css"
+  },
+  {
+    "revision": "a459a2820ad0a337af6b",
+    "url": "/static/js/2.08b2348c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.08b2348c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ed4777a57547a782f15",
+    "url": "/static/js/main.79925ed2.chunk.js"
+  },
+  {
+    "revision": "14bda32c0742ecea7e46",
+    "url": "/static/js/runtime-main.6f4528b4.js"
+  },
+  {
+    "revision": "fce8d1ea0495b9cc259fea5734be550e",
+    "url": "/static/media/book_demo.fce8d1ea.png"
+  },
+  {
+    "revision": "5b0bf5b10c4cdb8c0bd39bd1c448e823",
+    "url": "/static/media/home_banner.5b0bf5b1.png"
+  },
+  {
+    "revision": "44f64ecb3d3d9115f3f105acfa7029d7",
+    "url": "/static/media/logo.44f64ecb.svg"
+  },
+  {
+    "revision": "08f4f821ed150510ba48f71f77fd59b5",
+    "url": "/static/media/project_demo.08f4f821.png"
+  },
+  {
+    "revision": "801b20cedb50ebacd9096b5dd8316c96",
+    "url": "/static/media/qrcode_demo.801b20ce.png"
+  },
+  {
+    "revision": "5f5054bf1d9da6e821adeccd8c3c35ff",
+    "url": "/static/media/service_arrow.5f5054bf.svg"
+  },
+  {
+    "revision": "85b902ba8ab6aa544250ee0f8cb7d9d4",
+    "url": "/static/media/service_coach.85b902ba.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "756d6ad07a4c38b274fe72f7acb3399a",
+    "url": "/static/media/slider_arrow.756d6ad0.svg"
+  },
+  {
+    "revision": "881c87c6f8c7a6578b2446a9e3ef4e41",
+    "url": "/static/media/source_lesson.881c87c6.svg"
+  },
+  {
+    "revision": "7a7e248bdd2c2f0c5b87413769f0dc08",
+    "url": "/static/media/source_online.7a7e248b.svg"
+  },
+  {
+    "revision": "1c8c68eeb86c14137510da689b9c4301",
+    "url": "/static/media/source_publication.1c8c68ee.svg"
+  },
+  {
+    "revision": "243897c58336e3306e6d9eb00aee85be",
+    "url": "/static/media/video_icon_arrow.243897c5.svg"
+  },
+  {
+    "revision": "17a2a3e70cdc5be7b2887dcb0ae265fa",
+    "url": "/static/media/video_icon_play.17a2a3e7.svg"
+  },
+  {
+    "revision": "4a653619506bc3ae0f7f8f4829e27d62",
+    "url": "/static/media/video_poster.4a653619.png"
+  }
+]);
